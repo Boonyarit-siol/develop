c4d92ca991fdf05c551d88e9f3b1c5c69561860e
User <6210210299@email.psu.ac.th>
2023-01-20 15:03:32 +0700

    seafood menu update

--- lib/main.dart
+++ lib/main.dart
@@ -26,30 +26,33 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Menu> menu = [
-    Menu("Seafood", "700", "assets/images/Seafoods.png"),
-    Menu("Rabbit", "1000", "assets/images/rabbit.png"),
-    Menu("Kangaroo", "1200","assets/images/kangaroos.png"),
-    Menu("Chicken", "800","assets/images/chickens.png")
-  ];
+  // List<Menu> menu = [
+  //   Menu("Seafood", "700", "assets/images/Seafoods.png"),
+  //   Menu("Rabbit", "1000", "assets/images/rabbit.png"),
+  //   Menu("Kangaroo", "1200", "assets/images/kangaroos.png"),
+  //   Menu("Chicken", "800", "assets/images/chickens.png")
+  // ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("Seafood Samila "),
         ),
-        body: ListView.builder(
-            itemCount: menu.length,
-            itemBuilder: (BuildContext context, int index) {
-              Menu food = menu[index];
-              return ListTile(
-                leading:Image.asset(food.img),
-                title: Text(
-                  food.name,
-                  style: TextStyle(fontSize: 15),
-                ),
-                subtitle: Text("Money:" + food.money + "/baht"),
-              );
-            }));
+        // body: ListView.builder(
+        //     itemCount: menu.length,
+        //     itemBuilder: (BuildContext context, int index) {
+        //       Menu food = menu[index];
+        //       return ListTile(
+        //           leading: Image.asset(food.img),
+        //           title: Text(
+        //             food.name,
+        //             style: TextStyle(fontSize: 15),
+        //           ),
+        //           subtitle: Text("Money:" + food.money + "/baht"),
+        //           onTap: () {
+        //             print("You choose food menu:" + food.name);
+        //           });
+        //     }));
+    );
   }
 }
